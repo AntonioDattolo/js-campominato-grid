aae2dc5d830c4af7d4010e6b4653223ae79aaeb8
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-30 15:10:38 +0200

    MILESTONE 4

--- js/main.js
+++ js/main.js
@@ -2,14 +2,32 @@
 let grid = document.getElementById("my-grid");
 
 let play = document.getElementById("play");
+// MILESTONE 3
+// play.addEventListener( "click", function(){
 
-play.addEventListener( "click", function(){
-
-    for (let x=0; x < 100; x++){
-        let square = document.createElement("div"); 
-        grid.append(square)
-        square.classList.add("base-square");
-        square.classList.add("bg-square")
-        square.innerHTML = x;
+//     for (let x=0; x < 100; x++){
+//         let square = document.createElement("div"); 
+//         grid.append(square)
+//         square.classList.add("base-square");
+//         square.classList.add("bg-square")
+//         square.innerHTML = x;
+//     }
+// })
+//FUNZIONE PER DAR INIZIO AL CICLO DELLA CREAZIONE DELLA GRIGLIA
+function start(){
+    for (let x = 0; x < 100; x++) {
+        let gridCreations = createSquare(x)
     }
-})
+}
+// ***************FUNZIONE PER LA CREAZIONE DEI QUADRATI DELLA GRIGLIA
+function createSquare(y){
+    let square = document.createElement("div");
+    grid.append(square)
+    square.classList.add("base-square");
+    square.classList.add("bg-square")
+    square.innerHTML = y;
+    return square
+}
+
+// EVENTO CHE DA IL VIA ALLA CREAZIONE DELLA GRIGLIA
+play.addEventListener("click", start)
