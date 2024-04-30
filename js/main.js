ebd3aac70a33f745cd2034fb183c893b826da79d
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-30 14:56:51 +0200

    MILESTONE 3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 // let grid = document.querySelector('div');
 let grid = document.getElementById("my-grid");
-for (let x=0; x < 100; x++){
-    let square = document.createElement("div"); 
-    grid.append(square)
-    square.classList.add("base-square");
-    square.innerHTML = x;
-}
+
+let play = document.getElementById("play");
+
+play.addEventListener( "click", function(){
+
+    for (let x=0; x < 100; x++){
+        let square = document.createElement("div"); 
+        grid.append(square)
+        square.classList.add("base-square");
+        square.classList.add("bg-square")
+        square.innerHTML = x;
+    }
+})
